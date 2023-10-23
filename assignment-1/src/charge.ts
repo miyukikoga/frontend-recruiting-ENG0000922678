c397ca6447055462d2e6b33040dd82882cec2335
miyukikoga <>
2023-10-24 05:50:27 +0900

    課題1: リファクタリング

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -8,43 +8,91 @@ export type Receipt = {
   change: number;
 };
 
-export type Payment = {
-  type: string;
+type CashPayment = {
+  type: 'CASH';
+  amount: number;
+};
+
+type CouponPayment = {
+  type: 'COUPON';
   percentage?: number;
   amount?: number;
 };
 
+export type Payment = CashPayment | CouponPayment;
+
+/**
+ * 支払う
+ * @param invoice 請求書
+ * @param payments 支払い
+ * @returns レシート
+ */
 export function charge(invoice: Invoice, payments: Payment[]) {
   const total = invoice.total;
-  let deposit = 0;
 
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
-        if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
-        } else {
-          deposit += payment.amount || 0;
-        }
-      } else {
-        if (deposit >= total) {
-          throw new Error('OverCharge');
-        }
-        deposit += payment.amount || 0;
-      }
-    });
+  const deposit = payments
+    .sort((payment) => (isCouponPayment(payment) ? -1 : 1))
+    .reduce((sum: number, current: Payment) => {
+      if (sum >= total) throw new Error('OverCharge');
+      return sum + calculateDeposit(current, total);
+    }, 0);
   if (total > deposit) {
     throw new Error('Shortage');
   }
 
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
-  }
-  if (isCoupon) return { total, deposit, change: 0 };
-  return { total: total, deposit: deposit, change: deposit - total };
+  const hasPaymentsByCash = payments.some((payment) => !isCouponPayment(payment));
+  // 全ての支払いが商品券のとき、お釣りは発生しない
+  if (!hasPaymentsByCash) return createReceipt(total, deposit, 0);
+  return createReceipt(total, deposit, deposit - total);
 }
+
+/**
+ * 支払い方法が商品券か判定する
+ * 型ガードも行う
+ * @param type 支払い方法
+ * @returns 商品券かどうか
+ */
+const isCouponPayment = (payment: unknown): payment is CouponPayment => {
+  return (payment as CouponPayment).type === 'COUPON';
+};
+
+/**
+ * 支払額を計算する
+ * @param currentPayment 今回の支払い
+ * @param invoiceTotal 請求額合計
+ * @returns 支払額
+ */
+const calculateDeposit = (currentPayment: Payment, invoiceTotal: number): number => {
+  if (isCouponPayment(currentPayment))
+    return calculateDepositByCoupon(currentPayment.percentage, currentPayment.amount, invoiceTotal);
+  return currentPayment.amount;
+};
+
+/**
+ * 商品券による支払額を計算する
+ * @param couponPercentage
+ * @param deposit 支払い額
+ * @param invoiceTotal 請求額合計
+ * @returns 支払額
+ */
+const calculateDepositByCoupon = (
+  percentage: number | undefined,
+  amount: number | undefined,
+  invoiceTotal: number,
+): number => {
+  if (percentage != undefined) {
+    return Math.floor(invoiceTotal * (percentage / 100));
+  }
+  return amount || 0;
+};
+
+/**
+ * レシートを作成する
+ * @param total 請求額合計
+ * @param deposit 支払い額
+ * @param change お釣り
+ * @returns レシート
+ */
+const createReceipt = (total: number, deposit: number, change: number): Receipt => {
+  return { total, deposit, change };
+};
